6cc6d84d566a40426ff9b8f37b450c51f1222276
Wenjiacheng <1509463062@qq.com>
2022-11-14 21:56:11 +0800

    增加flink读取pravega模块

---
+++ KDC-Flink/src/main/java/com/mcla/realtime/bean/MCResource.java
@@ -0,0 +1,13 @@
+//package com.mcla.realtime.bean;
+//
+//import java.io.Serializable;
+//
+///**
+// * @Description: test
+// * @ClassName: TestStats
+// * @Author: ice_light
+// * @Date: 2022/11/14 17:50
+// * @Version: 1.0
+// */
+//public class MCResource implements Serializable {
+//}
